508ccaa0d00de2f3e9db87ae5846e50893aa3055
vedran-ns <vexleg@hotmail.com>
2026-02-18 17:35:14 +0100

    Update Authorization token to 'Bearer Token'

--- api/beluga-api-old.php
+++ api/beluga-api-old.php
@@ -286,7 +286,7 @@ function validate_with_external_api() {
           'method' => 'POST',
           'httpversion' => '1.0',
           'headers' => array(
-           'Authorization' => 'Bearer z17DZCRW9jjUwuG3uRNr',
+           'Authorization' => 'Bearer Token',
             'Content-Type' => 'application/json'),
           'body' => json_encode($args1, JSON_UNESCAPED_SLASHES)
            )
@@ -360,7 +360,7 @@ function validate_with_external_api() {
           'method' => 'POST',
           'httpversion' => '1.0',
           'headers' => array(
-           'Authorization' => 'Bearer z17DZCRW9jjUwuG3uRNr',
+           'Authorization' => 'Bearer Token',
             'Content-Type' => 'application/json'),
           'body' => json_encode($args_photos, JSON_UNESCAPED_SLASHES)
            )
@@ -923,7 +923,7 @@ function send_chat_to_doctor_api($order, $entry) {
           'method' => 'POST',
           'httpversion' => '1.0',
           'headers' => array(
-           'Authorization' => 'Bearer z17DZCRW9jjUwuG3uRNr',
+           'Authorization' => 'Bearer Token',
             'Content-Type' => 'application/json'),
           'body' => json_encode($chat_args, JSON_UNESCAPED_SLASHES)
            )
